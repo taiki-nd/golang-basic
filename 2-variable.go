4f85e7ac4bc3cada50ad9aaf0172285b0e31d98b
Taiki Noda <taiki.nd.real@gmail.com>
2022-01-06 00:00:41 +0900

    varと簡略系の使い分け

--- 2-variable.go
+++ 2-variable.go
@@ -2,19 +2,22 @@ package main
 
 import "fmt"
 
-func main() {
-	var (
-		i    int     = 1
-		f64  float64 = 1.2
-		s    string  = "test"
-		t, f bool    = true, false
-	)
+var (
+	i    int     = 1
+	f64  float64 = 1.2
+	s    string  = "test"
+	t, f bool    = true, false //関数内外両方で宣言が可能
+)
 
-	fmt.Println(i, f64, s, t, f)
-
+func sub() {
 	xi := 1
 	xf64 := 1.2
 	xs := "test"
 	xt, xf := true, false
-	fmt.Println(xi, xf64, xs, xt, xf)
+	fmt.Println(xi, xf64, xs, xt, xf) //関数内でのみ有効な書き方
+}
+
+func main() {
+	fmt.Println(i, f64, s, t, f)
+	sub()
 }
