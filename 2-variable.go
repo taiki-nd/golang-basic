4b64c4e6962c6542010adaa25df066c79047f64c
Taiki Noda <taiki.nd.real@gmail.com>
2022-01-05 23:47:31 +0900

    変数の宣言

---
+++ 2-variable.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var i int = 1
+	var f64 float64 = 1.2
+	var s string = "test"
+	var t bool = true
+	var f bool = false
+
+	fmt.Println(i, f64, s, t, f)
+}
