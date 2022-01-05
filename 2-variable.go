7dc171fe0e9028ddfb8a0aca1d96511ff6d78f90
Taiki Noda <taiki.nd.real@gmail.com>
2022-01-05 23:48:14 +0900

    bool型の省略形

--- 2-variable.go
+++ 2-variable.go
@@ -6,8 +6,7 @@ func main() {
 	var i int = 1
 	var f64 float64 = 1.2
 	var s string = "test"
-	var t bool = true
-	var f bool = false
+	var t, f bool = true, false
 
 	fmt.Println(i, f64, s, t, f)
 }
