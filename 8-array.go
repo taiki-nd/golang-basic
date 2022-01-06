4d7d7176aefdcc08484da465df90dd8a11904d46
Taiki Noda <taiki.nd.real@gmail.com>
2022-01-06 17:44:45 +0900

    配列に関しての記述

---
+++ 8-array.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a [2]int
+	a[0] = 100
+	a[1] = 200
+	fmt.Println(a)
+
+	var b [2]int = [2]int{100, 200}
+	//b = append(b, 300) エラーになる
+	fmt.Println(b)
+
+	var c []int = []int{100, 200} //スライス[]だとok
+	c = append(c, 300)
+	fmt.Println(b)
+
+}
