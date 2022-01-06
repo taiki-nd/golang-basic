f0c99a465ec7395e7260f0505e405b6f0f961c17
Taiki Noda <taiki.nd.real@gmail.com>
2022-01-06 16:03:35 +0900

    簡略型の計算

--- 4-numeric.go
+++ 4-numeric.go
@@ -14,13 +14,22 @@ func main() {
 		fmt.Println(u8, i8, f32, c64)
 		fmt.Printf("type=%T value=%v", u8, u8)
 	*/
+	/*
+		fmt.Println("1 + 1 =", 1+1)
+		fmt.Println("10 - 1 =", 10-1)
+		fmt.Println("10 / 2 =", 10/2)
+		fmt.Println("10 / 3 =", 10/3)
+		fmt.Println("10.0 / 3 =", 10.0/3)
+		fmt.Println("10 / 3.0 =", 10/3.0)
+		fmt.Println("10 % 2 =", 10%2)
+		fmt.Println("10 % 3 =", 10%3)
+	*/
 
-	fmt.Println("1 + 1 =", 1+1)
-	fmt.Println("10 - 1 =", 10-1)
-	fmt.Println("10 / 2 =", 10/2)
-	fmt.Println("10 / 3 =", 10/3)
-	fmt.Println("10.0 / 3 =", 10.0/3)
-	fmt.Println("10 / 3.0 =", 10/3.0)
-	fmt.Println("10 % 2 =", 10%2)
-	fmt.Println("10 % 3 =", 10%3)
+	x := 0
+	fmt.Println(x)
+	x++
+	fmt.Println(x)
+	x--
+	fmt.Println(x)
+
 }
